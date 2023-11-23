e5e2c6641032acda36be0370bf566914820a28f9
Mohammad suhail <skhan282290@gmail.com>
2023-11-23 10:14:39 +0530

    auth

--- actions/getSession.ts
+++ actions/getSession.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
 
 export default async function getSession() {
